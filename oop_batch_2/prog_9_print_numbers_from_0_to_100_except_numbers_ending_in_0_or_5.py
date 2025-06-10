85120dc2822de4b974feca8397745f700c85de6c
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-06-10 17:47:51 +0800

    added a funtion in the class that divides the numbers with 5. and if the number cannot be divided by 5, then it gets pprinted out.

--- oop_batch_2/prog_9_print_numbers_from_0_to_100_except_numbers_ending_in_0_or_5.py
+++ oop_batch_2/prog_9_print_numbers_from_0_to_100_except_numbers_ending_in_0_or_5.py
@@ -1,1 +1,14 @@
 # Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
+# create a class 
+# create a function inside the class that will sort out numbers ending in zero or in five
+# make sure that only numbers that arent ending in 0 nor 5 gets printed
+
+class NumberPrinter:
+    def number_sorter():
+        for i in range(0, 101):
+            if i % 5 != 0:
+                print(i)
+
+number = NumberPrinter
+number.number_sorter()

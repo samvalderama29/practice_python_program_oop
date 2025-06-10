8e16aa221960b388e35bfd425baf0a080b303b6f
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-06-10 17:33:13 +0800

    added a function in the class that will sort out anf print only the even numbers.

--- oop_batch_1/prog_9_print_even_numbers_from_1_to_100.py
+++ oop_batch_1/prog_9_print_even_numbers_from_1_to_100.py
@@ -1,1 +1,14 @@
 # Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+# ask user to proceed
+# make a function that will print out the even numbers
+# print shit
+
+class EvenNumbers:
+    def even_sort():
+        for i in range(1, 101):
+            if i % 2 == 0:
+                print(i)
+
+even_shit = EvenNumbers
+even_shit.even_sort()

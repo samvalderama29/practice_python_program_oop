916e1ad8131fdadc4cc3aa01de5dbd7c0e64bcd3
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-06-10 17:39:25 +0800

    added a funtion inside the class that will sort out the numebrs that ends in 0. only numbers that does not end in 0 will get printed out.

--- oop_batch_1/prog_10_print_numbers_from_0_to_100_except_numbers_ending_in_zero.py
+++ oop_batch_1/prog_10_print_numbers_from_0_to_100_except_numbers_ending_in_zero.py
@@ -1,1 +1,14 @@
 # Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+# create a class
+# create a funtion inside the class that will be responsible for sorting out the numbers that ends with 0
+# print the numbers that arent ending in 0
+
+class NoZero:
+    def zero_sorter():
+        for i in range(1, 101):
+            if i % 10 != 0:
+                print(i)
+
+no_zero = NoZero
+no_zero.zero_sorter()

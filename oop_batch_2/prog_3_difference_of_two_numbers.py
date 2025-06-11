264954c81df03ff4ae8805c1e7b5e3ab3b9efecc
kok0krunch <amaliakadoi@gmail.com>
2025-06-11 16:26:34 +0800

    Create a class and a function that prompts the user for two numbers

---
+++ oop_batch_2/prog_3_difference_of_two_numbers.py
@@ -0,0 +1,14 @@
+# Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+# Create a class
+# Create a function that will ask the user to input two numbers
+# Create a function that will calculate the difference of the two numbers
+
+class DifferenceOfTwoNumbers:
+    def user_num(self):
+        try:
+            self.num_1 = int(input('Enter the first number: '))
+            self.num_2 = int(input('Enter the second number: '))
+        except ValueError:
+            print('Invalid input. Please enter numbers only.')
+            self.user_num()

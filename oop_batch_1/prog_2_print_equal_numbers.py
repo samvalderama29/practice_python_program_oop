c3118b7bc6e4ed87938f7d3062750b9ff564025c
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-06-11 09:15:34 +0800

    created a class and a function under the program that will identify if numbers are 'Equal'; use try-except method once user input is not a number and an if-else method to identify if numbers are the same

--- oop_batch_1/prog_2_print_equal_numbers.py
+++ oop_batch_1/prog_2_print_equal_numbers.py
@@ -1,1 +1,24 @@
 # Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+# Create a class
+# Create a function that will ask the user to input two numbers
+# Create a function with an if-else method inside to identify if the numbers are equal
+# Print "Equal" if the number are the same, else, print "Invalid. Numbers are not equal."
+
+class EqualNumber:
+    def user_num(self):
+        try:
+            self.num_1 = int(input('Enter a number: '))
+            self.num_2 = int(input('Enter another number: '))
+        except ValueError:
+            print('Invalid. Please enter numbers only')
+        exit()
+    def equal_num(self):
+        if self.num_1 == self.num_2:
+            print('Equal')
+        else:
+            print('Invalid. Numbers are not equal.')
+
+print_equal_num = EqualNumber()
+print_equal_num.user_num()
+print_equal_num.equal_num()

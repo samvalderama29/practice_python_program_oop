ac6749df5e6811eb414d967f17ed4ebff6b8b4d6
kok0krunch <amaliakadoi@gmail.com>
2025-06-11 16:04:06 +0800

    Create class for sum of two numbers

---
+++ oop_batch_1/prog_3_print_sum_of_two_numbers.py
@@ -0,0 +1,8 @@
+# Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+# Create a class
+# Create a function that will ask the user to input two numbers
+# Create a function that will calculate the sum of the two numbers
+# Print the sum of the two numbers
+
+class SumOfTwoNumbers:

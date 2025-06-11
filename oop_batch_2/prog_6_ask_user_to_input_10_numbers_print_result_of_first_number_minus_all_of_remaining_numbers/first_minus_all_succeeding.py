a41c87e8aab360c2750693637d7523a3a53bd475
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-11 18:21:02 +0800

    remove the self.total and instead made the total variable a global variable.

--- oop_batch_2/prog_6_ask_user_to_input_10_numbers_print_result_of_first_number_minus_all_of_remaining_numbers/first_minus_all_succeeding.py
+++ oop_batch_2/prog_6_ask_user_to_input_10_numbers_print_result_of_first_number_minus_all_of_remaining_numbers/first_minus_all_succeeding.py
@@ -1,14 +1,14 @@
 class OneMinusAll:
-    def __init__(self, first_number,total):
+    def __init__(self, first_number):
         self.first_number=first_number
         self.suceeding_numbers=[]
-        self.total=total#Find total of the other 9 numbers
     
     def first_minus_succeeding(self):
-        difference= self.first_number-self.total#subtract sum of the 9 numbers to the first number
+        difference= self.first_number-total#subtract sum of the 9 numbers to the first number
         print("\ndifference:", difference) #Print their difference
     
     def total_of_succeeding(self):
+        global total
         counter= 1
         total=0
         while counter!=10:#Ask user to input other 9 numbers
@@ -17,3 +17,4 @@ class OneMinusAll:
             self.suceeding_numbers.append(other_numbers)
             counter= counter+1 
             total= total+other_numbers
+        total=sum(self.suceeding_numbers)

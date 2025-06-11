4242689ae9650ca62f4f2c7a4a6d08e5ce5a1692
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-06-11 09:19:20 +0800

    created a class and added a function that allows the user to input numbers and determine which one is smaller using the if-else method

--- oop_batch_2/prog_1_print_smaller_number.py
+++ oop_batch_2/prog_1_print_smaller_number.py
@@ -1,1 +1,21 @@
 # Create a program that ask user to input 2 numbers. Print the smaller number.
+
+# Ask the user to input two numbers
+# Make a function that will compare the two numbers using if-else and determine which one is smaller
+# Print the smaller number
+
+class SmallerNumber:
+    def user_num(self):
+        self.num_1 = int(input('Enter a number: '))
+        self.num_2 = int(input('Enter another number: '))
+    def smaller_num(self):
+        if self.num_1 > self.num_2:
+            print(f'The smaller number is {self.num_2}')
+        elif self.num_1 < self.num_2:
+            print(f'The smaller number is {self.num_1}')
+        else:
+            print('Both numbers are equal. It is not possible to determine which one is smaller.')
+
+print_small_num = SmallerNumber()
+print_small_num.user_num()
+print_small_num.smaller_num()

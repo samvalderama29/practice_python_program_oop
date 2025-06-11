413afca6347db0efb97067c6c1ec6b746edfd64b
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-11 17:46:22 +0800

    imported class into main program, used the input function so user can input value of first and second number and called the definition inside the class.

--- oop_batch_2/prog_5_ask_user_to_input_2_numbers_print_remainder_when_first_number_is_divided_by_second_number/main_program.py
+++ oop_batch_2/prog_5_ask_user_to_input_2_numbers_print_remainder_when_first_number_is_divided_by_second_number/main_program.py
@@ -1,3 +1,5 @@
-first_number= int(input("Enter first number:"))
-second_number= int(input("Enter second number:"))
- 
+from divide_and_find_remainder import Remainder
+
+if __name__ == "__main__":
+    numbers=Remainder(float(input("Enter first number:")), float(input("Enter second number:")))
+    numbers.modulo()

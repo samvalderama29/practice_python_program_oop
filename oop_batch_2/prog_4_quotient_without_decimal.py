61cd37bab1475cceaf14a003fdbbf0cdd87e6919
kok0krunch <amaliakadoi@gmail.com>
2025-06-11 16:34:08 +0800

    Create a class and function that prompts user for two numbers to divide

---
+++ oop_batch_2/prog_4_quotient_without_decimal.py
@@ -0,0 +1,17 @@
+# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point.
+
+# Create a class
+# Create a function that will ask the user to input two numbers
+# Create a function that will calculate the quotient of the two numbers without the decimal point
+
+class QuotientWithoutDecimal:
+    def user_num(self):
+        try:
+            self.num_1 = int(input("Enter the first number: "))
+            self.num_2 = int(input("Enter the second number: "))
+            if self.num_2 == 0:
+                print("Division by zero is not allowed. Please enter a non-zero second number.")
+                self.user_num()
+        except ValueError:
+            print("Invalid input. Please enter numbers only.")
+            self.user_num()

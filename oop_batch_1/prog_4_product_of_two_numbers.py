7b3961f3e7f88dc66a2f583f51d5a817fe35a746
kok0krunch <amaliakadoi@gmail.com>
2025-06-11 16:18:08 +0800

    Create class for product of two numbers

---
+++ oop_batch_1/prog_4_product_of_two_numbers.py
@@ -0,0 +1,7 @@
+# Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+# Create a class
+# Create a function that will ask the user to input two numbers
+# Create a function that will calculate the product of the two numbers
+
+class ProductOfTwoNumbers:

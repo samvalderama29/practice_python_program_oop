741a3bd52a32b7c463a6639f585812043e3e2a84
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-06-11 09:01:41 +0800

    created a class and added a function that allows the user to input numbers and determine which one is larger using the if-else method

--- oop_batch_1/prog_1_print_bigger_number.py
+++ oop_batch_1/prog_1_print_bigger_number.py
@@ -1,1 +1,21 @@
 # Create a program that ask user to input 2 numbers. Print the bigger number.
+
+# Ask the user to input two numbers
+# Make a function that will compare the two numbers using if-else and determine which one is bigger
+# Print the bigger number
+
+class BiggerNumber:
+    def user_num(self):
+        self.num_1 = int(input('Enter a number: '))
+        self.num_2 = int(input('Enter another number: '))
+    def bigger_num(self):
+        if self.num_1 > self.num_2:
+            print(f"The bigger number is {self.num_1}")
+        elif self.num_1 < self.num_2:
+            print(f"The bigger number is {self.num_2}")
+        else:
+            print("Both numbers are equal.")
+
+print_big_num = BiggerNumber()
+print_big_num.user_num()
+print_big_num.bigger_num()

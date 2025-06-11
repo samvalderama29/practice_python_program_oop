1a8aaea558dfd6d67ed74cee177f384e193fc9fe
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-11 16:40:53 +0800

    added parentheis, because it didn't work before.

--- oop_batch_1/prog_5_ask_user_to_input_2_numbers_print_quotient_of_two_numbers_with_decimal_point/main_program.py
+++ oop_batch_1/prog_5_ask_user_to_input_2_numbers_print_quotient_of_two_numbers_with_decimal_point/main_program.py
@@ -2,4 +2,4 @@ from divide_and_flash_answer_with_decimal_point import QuotientWithDecimal
 
 if __name__ == "__main__":
     numbers=QuotientWithDecimal(float(input("Enter first number:")), float(input("Enter second number:")))
-    numbers.divide
+    numbers.divide()
